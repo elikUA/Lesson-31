eda773f7c4680ac8c732610622b8c32aeb1c90fc
mainsroft.www <93188370+elikUA@users.noreply.github.com>
2022-01-18 13:38:07 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+ let a =document.querySelector('h1')
+ let b =document.querySelector('h1')
+ a.style.color = 'black'
+
+ function colorChanger () {
+     a.style.color = 'red'
+ }
+
+ function sizeChanger () {
+     a.style.fontSize = '100px'
+ }
+
+
+//  доп.зд
+//  let c,d
+//  c =+prompt('l=')
+//  d =c*3.14
+//  alert('длинна='+d)
+//  console.log('длинна=',d)
+
+
+// доп.зд(2)
+// let e,f,g,t,u,y
+// e =+prompt('e=')
+// f =+prompt('f=')
+// g =e+f
+// t =e-f
+// u =e*f
+// y =e/f
+// console.log('калькулятор решил',g, '-плюс;',t, '-минус;',u, '-умножить;',y, '-диление;')
+
+
+// let z = document.querySelector('img')
+// let x = document.addEventListener('keydown', function(){
+//     document.addEventListener('keydown',function(){
+//         z.classList.remove('jump')
+//     })
+//     z.classList.add('jump')
+// })
+
+
+
